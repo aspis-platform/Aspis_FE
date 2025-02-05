d08df2d36e727d2abf3ce4e5a4323ace726a2588
Sohee Kim <ttohee1275@gmail.com>
2025-02-05 13:25:21 +0900

    hompage-main 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import aspisLogo from './assets/aspis-logo.svg'
-import './App.css'
+import "./App.css";
+import { Outlet, Route, Routes } from "react-router-dom";
+import NavBar from "./components/homepage/NavBar";
+import Footer from "./components/homepage/Footer";
+import HomepageMain from "./pages/homepage/HomepageMain";
+import styled from "styled-components";
+
+function Layout() {
+  return (
+    <StyledDiv>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <>
-      <h1>Aspis - 여수 여미지보호협회 관리 서비스</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<HomepageMain />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
