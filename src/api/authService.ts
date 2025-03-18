8ccf9765f3e7a3c696414dd33de236c4808dc3fd
Sohee Kim <ttohee1275@gmail.com>
2025-03-18 23:12:15 +0900

    feat: create instances

---
+++ src/api/authService.ts
@@ -0,0 +1,64 @@
+import { AxiosError } from "axios";
+import instances, { login } from "./axios";
+import { toast } from "react-toastify";
+
+interface data {
+  name: string;
+  password: string;
+  email: string;
+}
+
+export class AuthService {
+  static async login(name: string, password: string) {
+    try {
+      const response = await instances["auth"].post<{
+        access_token: string;
+        refresh_token: string;
+      }>(name, password);
+      login(response.data.access_token, response.data.refresh_token);
+    } catch (error) {
+      if (!(error instanceof AxiosError)) return;
+
+      switch (error.response?.status) {
+        case 401:
+          toast.error("이름 또는 비밀번호가 틀렸습니다.");
+          break;
+        case 404:
+          toast.error("존재하지 않는 사용자입니다");
+          break;
+        default:
+          toast.error("로그인 중 오류가 발생했습니다.");
+          break;
+      }
+    }
+  }
+
+  static async register(name: string, password: string, email: string) {
+    const body = {
+      name,
+      password,
+      email,
+    };
+
+    try {
+      await instances["auth"].post("/auth/register", body);
+      toast.success("회원가입이 완료되었습니다. 로그인해주세요.");
+      return true;
+    } catch (error) {
+      if (!(error instanceof AxiosError)) return false;
+
+      switch (error.response?.status) {
+        case 409:
+          toast.error("이미 존재하는 사용자입니다.");
+          break;
+        case 401:
+          toast.error("가입할 수 없습니다");
+          break;
+        default:
+          toast.error("회원가입 중 오류가 발생했습니다.");
+          break;
+      }
+      return false;
+    }
+  }
+}
---
+++ src/api/axios.ts
@@ -0,0 +1,71 @@
+import axios, { AxiosError, AxiosInstance } from "axios";
+import { Cookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+type ServerType = "auth" | "core" | "upload" | "ai";
+
+const cookies = new Cookies();
+const instances: Record<ServerType, AxiosInstance> = <
+  Record<ServerType, AxiosInstance>
+>{};
+
+function login(accessToken: string, refreshToken: string | undefined) {
+  cookies.set("accessToken", accessToken);
+  if (refreshToken) cookies.set("refreshToken", refreshToken);
+}
+
+function logout() {
+  cookies.remove("accessToken");
+  cookies.remove("refreshToken");
+}
+
+function createInstance(type: ServerType, baseUrl: string) {
+  const instance = axios.create({
+    baseURL: baseUrl,
+  });
+
+  instance.interceptors.request.use((config) => {
+    config.headers.Authorization = cookies.get("accessToken");
+    return config;
+  });
+
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (!(error instanceof AxiosError)) return;
+      if (error.status === 401) {
+        const refreshToken = cookies.get("refreshToken");
+        if (refreshToken) {
+          try {
+            const response = await axios.post(
+              `${import.meta.env.AUTH_URL}/auth/reissue`,
+              {
+                refresh_token: refreshToken,
+              }
+            );
+            login(response.data.accessToken, undefined);
+          } catch (error) {
+            toast.error("다시 로그인하세요.");
+            useNavigate()("/login");
+          }
+        }
+      } else if (error.status === 500) {
+        toast.error("펑!@!@! << 서버터지는소리");
+      } else {
+        throw error;
+      }
+    }
+  );
+
+  instances[type] = instance;
+}
+
+createInstance("auth", import.meta.env.AUTH_URL);
+createInstance("core", import.meta.env.CORE_URL);
+createInstance("upload", import.meta.env.UPLOAD_URL);
+createInstance("ai", import.meta.env.AI_URL);
+
+export { login, logout };
+export type { ServerType };
+export default instances;
